7473d5e6ed61b1370ad2506e11e1ba99b52d7868
Kth <vcro12123@naver.com>
2022-03-16 17:46:57 +0900

    :sparkles:   feat   Tap 기능 추가

--- src/page/Main.vue
+++ src/page/Main.vue
@@ -1,11 +1,50 @@
 <template>
-  <h1>Hello! last project!!</h1>
+  <div>
+    <h1>Hello! last project!!</h1>
+    <TabPart :tabs="tabs" :currentTab="currentTab" @changeTab="changeTab" />
+  </div>
 </template>
 
 <script>
+import TabPart from "../components/TabPart.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    TabPart,
+  },
+  data() {
+    return {
+      currentTab: 0,
+      tabs: [
+        {
+          name: "모두",
+          icon: {
+            src: require("../assets/tab1.png"),
+            alt: "companyMe",
+          },
+        },
+        {
+          name: "본인",
+          icon: {
+            src: require("../assets/tab2.png"),
+            alt: "Me",
+          },
+        },
+        {
+          name: "회사",
+          icon: {
+            src: require("../assets/tab3.png"),
+            alt: "company",
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    changeTab(i) {
+      this.currentTab = i;
+    },
+  },
 };
 </script>
 
