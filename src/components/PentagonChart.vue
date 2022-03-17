02051576d94e2d9da1d0c78c182674cf33a1efb5
sunysty <seokjiseon@gmail.com>
2022-03-17 09:03:05 +0900

    ✨ Feat : 펜타곤차트 탭 마다 그래프 바뀌는 기능

--- src/components/PentagonChart.vue
+++ src/components/PentagonChart.vue
@@ -1,12 +1,12 @@
 <template>
-  <RadarChart :chartData="matchData" :options="options" />
+  <RadarChart :chartData="resultData" />
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import { RadarChart } from "vue-chart-3";
 import { Chart, registerables } from "chart.js";
-// import Data from "../Data";
+import Data from "../Data";
 
 Chart.register(...registerables);
 
@@ -14,43 +14,108 @@ export default defineComponent({
   name: "APP",
   components: { RadarChart },
   setup() {
-    const matchData = {
+    const setData = {
       labels: ["적극적인", "자신있는", "책임감있는", "개인주의", "수평적인"],
       datasets: [
         {
           label: "본인",
-          data: [8, 10, 10, 3, 6],
+          data: Data.user,
           backgroundColor: "rgba(110, 60, 249, 0.32)",
           borderColor: "#6E3CF9",
         },
         {
           label: "회사",
-          data: [8, 10, 20, 3, 6],
+          // data: Data.company,
           background: "rgba(255, 193, 74, 0.32)",
           borderColor: "#FFD335",
         },
+        {
+          label: "외곽선",
+          data: [10, 10, 10, 10, 10],
+          backgroundColor: "rgba(248, 248, 248, 0.32)",
+          borderColor: " #B2B2B2",
+          pointRadius: 13,
+          borderWidth: 1,
+          pointBackgroundColor: [
+            "rgba(235, 133, 108, 0.7)",
+            "rgba(108, 135, 245, 0.5)",
+            "rgba(64, 171, 199, 0.5)",
+            "rgba(229, 115, 160, 0.5)",
+            "rgba(101, 184, 81, 0.5)",
+          ],
+          pointBorderColor: [
+            "rgba(235, 133, 108, 0.7)",
+            "rgba(108, 135, 245, 0.5)",
+            "rgba(64, 171, 199, 0.5)",
+            "rgba(229, 115, 160, 0.5)",
+            "rgba(101, 184, 81, 0.5)",
+          ],
+          pointBorderWidth: 1,
+          pointHoverRadius: 1,
+        },
       ],
     };
-    const options = {};
-    return { matchData, options };
+    return { setData };
+  },
+  beforeMount() {
+    const { scale, plugins } = Chart.defaults;
+    scale.ticks = {
+      stepSize: 2,
+      display: false,
+    };
+    scale.grid.borderDash = [5];
+    plugins.legend.display = false;
+    plugins.tooltip.enabled = false;
+  },
+  data() {
+    return { resultData: this.setData };
+  },
+  props: {
+    company: String,
+    currentTab: Number,
   },
   methods: {
     show(idx) {
-      this.matchData.datasets[idx].fill = true;
-      this.matchData.datasets[idx].showLine = true;
+      this.resultData.datasets[idx].fill = true;
+      this.resultData.datasets[idx].showLine = true;
     },
     hide(idx) {
-      this.matchData.datasets[idx].fill = false;
-      this.matchData.datasets[idx].showLine = false;
+      this.resultData.datasets[idx].fill = false;
+      this.resultData.datasets[idx].showLine = false;
     },
   },
-  beforeMount() {
-    const { scale } = Chart.defaults;
-    // plugins.legend.display = false;
-    // plugins.tooltip.enabled = false;
-
-    scale.grid.borderDash = [5];
+  beforeUpdate() {
+    const tabConvert = {
+      tab: {
+        //모두
+        0: (searchCompany) => {
+          this.resultData.datasets[0].data = [...Data["user"]];
+          this.resultData.datasets[1].data = [...Data[searchCompany]];
+          this.show(0);
+          this.show(1);
+        },
+        //유저
+        1: () => {
+          this.resultData.datasets[0].data = [...Data["user"]];
+          this.show(0);
+          this.hide(1);
+        },
+        //회사
+        2: (searchCompany) => {
+          this.resultData.datasets[1].data = [...Data[searchCompany]];
+          this.hide(0);
+          this.show(1);
+        },
+      },
+      convert(tabNum, searchCompany) {
+        searchCompany ? this.tab[tabNum] : this.tab[1]();
+      },
+    };
+    try {
+      tabConvert.convert(this.currentTab, this.company);
+    } catch (e) {
+      console.log(e);
+    }
   },
-  beforeUpdate() {},
 });
 </script>
--- src/page/Main.vue
+++ src/page/Main.vue
@@ -1,53 +1,15 @@
 <template>
   <div>
-    <h1>Hello! last project!!</h1>
     <PentagonChart />
-    <TabPart :tabs="tabs" :currentTab="currentTab" @changeTab="changeTab" />
   </div>
 </template>
 
 <script>
 import PentagonChart from "../components/PentagonChart";
-import TabPart from "../components/TabPart.vue";
+
 export default {
   name: "App",
-  components: {
-    TabPart,
-    PentagonChart,
-  },
-  data() {
-    return {
-      currentTab: 0,
-      tabs: [
-        {
-          name: "모두",
-          icon: {
-            src: require("../assets/tab1.png"),
-            alt: "companyMe",
-          },
-        },
-        {
-          name: "본인",
-          icon: {
-            src: require("../assets/tab2.png"),
-            alt: "Me",
-          },
-        },
-        {
-          name: "회사",
-          icon: {
-            src: require("../assets/tab3.png"),
-            alt: "company",
-          },
-        },
-      ],
-    };
-  },
-  methods: {
-    changeTab(i) {
-      this.currentTab = i;
-    },
-  },
+  components: { PentagonChart },
 };
 </script>
 
